ea9ae434b6c340308a03012b28e5d1bd17b3ff8e
JTexpo <42153049+JTexpo@users.noreply.github.com>
2023-05-18 12:27:44 -0500

    Spring Cleaning 2023
    Removed python code that does not well reflect me as a developer as of spring 2023

--- Google_Chrome_Dino_AI/read_me.txt
+++
@@ -1,4 +0,0 @@
-Please make sure that you have pip installed all the required files for the code
-
-To start do :
-python3 Game.py
--- Python_Automate_The_Boring_Stuff-master/Comma_Code.py
+++
@@ -1,5 +0,0 @@
-spam = ['apple','bananas','tofu','cats']
-
-if __name__ == '__main__':
-    first_str = ', '.join(spam[:-1])
-    print('{}, and {}'.format(first_str,spam[-1]))
